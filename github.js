c90082e9dbbfbb2f3bad76b0daff4ef4fe2bbadc
kesh25 <kinyua.dn@outlook.com>
2020-06-12 21:44:17 -0400

    initial commit

---
+++ github.js
@@ -0,0 +1,21 @@
+class Github {
+    constructor() {
+        this.client_id = '99480ba5b938113680a8';
+        this.client_secret = '03732da9589d5d6b1f4db9055bcc7a45d161237d';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
